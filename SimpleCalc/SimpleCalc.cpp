83ae34e01afa23c09f486325006d753668c1739e
anthony keel <farmtireguy@gmail.com>
2021-01-05 14:20:14 -0600

    user input added division by zero handled
    added user input and handled division  by zero

--- SimpleCalc/SimpleCalc.cpp
+++ SimpleCalc/SimpleCalc.cpp
@@ -6,6 +6,23 @@
 
 int main()
 {
-    
+    double x = 0.0, y = 0.0, result = 0.0;
+    char oper;
+
+    std::cout << "Simple calculator app " << std::endl << std::endl;
+    std::cout << "Enter the equation (Ex. a+b | a-b | a*b | a/b):" << std::endl;
+
+    calculate c;
+    while (true) {
+        std::cin >> x >> oper >> y;
+        if (y == 0 && oper == '/') {
+            std::cout << "Division by zero" << std::endl;
+        }
+        else {
+            result = c.Calculate(x, oper, y);
+            std::cout << "The result is: " << result << std::endl;
+        }
+    }
+    return 0;
 }
 
